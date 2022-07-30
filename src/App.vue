a917b74fe1504a1361e158af4e299935ba3826f8
Rafael Anjos Santos <raphaelsantosajs@gmail.com>
2022-07-29 22:03:45 -0300

    'final'

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
   <nav class="bg-dark">
     <form class="d-flex" role="search">
       <span class="text-white">Informe um CEP:</span>
-      <input id="cep" class="form-control me-2" type="search" placeholder="Ex: 12345-678" aria-label="Search"
-        v-model="cep">
-      <button class="btn btn-outline-success" type="submit" v-on:click="consult">Consultar</button>
+      <input id="cep" class="form-control me-2" type="search" placeholder="Ex: 12345-678" v-maska="'#####-###'"
+        aria-label="Search" v-model="Dados.cep">
+      <button class="btn btn-outline-success" type="submit" v-on:click.prevent="consult">Consultar <i
+          class="bi bi-search"></i></button>
     </form>
   </nav>
 
@@ -14,40 +15,42 @@
       <div class="duo-blocks">
         <div>
           Logradouro
-          <input type="text" class="form-control" aria-label="Sizing example input" id="Logradouro" v-model="logradouro"
-            readonly>
+          <input type="text" class="form-control" aria-label="Sizing example input" placeholder="-----" id="Logradouro"
+            v-model="Dados.logradouro" readonly>
         </div>
 
         <div>
           Complemento
-          <input type="text" class="form-control" aria-label="Sizing example input" id="Complemento"
-            v-model="complemento" readonly>
+          <input type="text" class="form-control" aria-label="Sizing example input" placeholder="-----" id="Complemento"
+            v-model="Dados.complemento" readonly>
         </div>
       </div>
 
       <div class="duo-blocks">
         <div>
           Bairro
-          <input type="text" class="form-control" aria-label="Sizing example input" id="Bairro" v-model="bairro"
-            readonly>
+          <input type="text" class="form-control" aria-label="Sizing example input" placeholder="-----" id="Bairro"
+            v-model="Dados.bairro" readonly>
         </div>
 
         <div>
           Localidade
-          <input type="text" class="form-control" aria-label="Sizing example input" id="Localidade" v-model="localidade"
-            readonly>
+          <input type="text" class="form-control" aria-label="Sizing example input" placeholder="-----" id="Localidade"
+            v-model="Dados.localidade" readonly>
         </div>
       </div>
 
       <div class="duo-blocks">
         <div>
           UF
-          <input type="text" class="form-control" aria-label="Sizing example input" id="UF" v-model="uf" readonly>
+          <input type="text" class="form-control" aria-label="Sizing example input" placeholder="-----" id="UF"
+            v-model="Dados.uf" readonly>
         </div>
 
         <div>
           IBGE
-          <input type="text" class="form-control" aria-label="Sizing example input" id="IBGE" v-model="ibge" readonly>
+          <input type="text" class="form-control" aria-label="Sizing example input" placeholder="-----" id="IBGE"
+            v-model="Dados.ibge" readonly>
         </div>
       </div>
 
@@ -55,58 +58,57 @@
       <div class="three-blocks">
         <div>
           GIA
-          <input type="text" class="form-control" aria-label="Sizing example input" id="GIA" v-model="gia" readonly>
+          <input type="text" class="form-control" aria-label="Sizing example input" placeholder="-----" id="GIA"
+            v-model="Dados.gia" readonly>
         </div>
 
         <div>
           DDD
-          <input type="text" class="form-control" aria-label="Sizing example input" id="DDD" v-model="ddd" readonly>
+          <input type="text" class="form-control" aria-label="Sizing example input" placeholder="-----" id="DDD"
+            v-model="Dados.ddd" readonly>
         </div>
 
         <div>
           SIAFI
-          <input type="text" class="form-control" aria-label="Sizing example input" id="SIAFI" v-model="siafi" readonly>
+          <input type="text" class="form-control" aria-label="Sizing example input" placeholder="-----" id="SIAFI"
+            v-model="Dados.siafi" readonly>
         </div>
       </div>
 
     </section>
   </main>
-
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25061235.470727414!2d-53.29014935068654!3d-14.970935823514925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9c59c7ebcc28cf%3A0x295a1506f2293e63!2sBrasil!5e0!3m2!1spt-BR!2sbr!4v1659142100472!5m2!1spt-BR!2sbr"
+    width="100%" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+  </iframe>
 </template>
 
 <script>
 import axios from 'axios'
+import { maska } from 'maska'
 
 export default {
   data() {
     return {
-      cep: '',
-      logradouro: '',
-      complemento: '',
-      bairro: '',
-      localidade: '',
-      uf: '',
-      ibge: '',
-      gia: '',
-      ddd: '',
-      siafi: ''
+      Dados: {}
     }
   },
+  directives: { maska },
   methods: {
     consult() {
       console.log(this.cep)
       axios
-        .get(`https://viacep.com.br/ws/${this.cep}/json`)
-        .then(({data}) => {
-          this.bairro = data.bairro
-          this.logradouro = data.logradouro
-          this.complemento = data.complemento
-          this.localidade = data.localidade
-          this.uf = data.uf
-          this.ibge = data.ibge
-          this.gia = data.gia
-          this.ddd = data.ddd
-          this.siafi = data.siafi
+        .get(`https://viacep.com.br/ws/${this.Dados.cep}/json`)
+        .then(({ data }) => {
+          this.Dados.bairro = data.bairro
+          this.Dados.logradouro = data.logradouro
+          this.Dados.complemento = data.complemento
+          this.Dados.localidade = data.localidade
+          this.Dados.uf = data.uf
+          this.Dados.ibge = data.ibge
+          this.Dados.gia = data.gia
+          this.Dados.ddd = data.ddd
+          this.Dados.siafi = data.siafi
         })
     }
   }
