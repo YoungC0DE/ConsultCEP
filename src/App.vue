a003d95b4a213d954820106064ed2bffc9efd17e
Rafael Anjos Santos <68437256+YoungC0DE@users.noreply.github.com>
2023-04-22 17:54:06 -0300

    improviments

--- src/App.vue
+++ src/App.vue
@@ -1,146 +1,70 @@
 <template>
   <main>
-    <div
-      class="d-flex flex-column align-items-center justify-content-center gap-2"
-    >
-      <nav class="bg-dark">
-        <span class="text-white">Informe o CEP:</span>
-        <input
-          id="cep"
-          class="form-control"
-          type="search"
-          placeholder="Ex: 12345-678"
-          v-maska="'#####-###'"
-          required
-          autofocus
-          autocomplete="off"
-          v-model="CEP"
-        />
-        <button
-          class="btn btn-outline-success"
-          type="button"
-          v-on:click="consult"
-        >
-          Consultar
-        </button>
+    <div class="main-container">
+
+      <nav class="d-flex flex-column gap-2">
+        <span>Informe o CEP:</span>
+        <div class="d-flex flex-row search-section">
+          <input id="cep" class="form-control rounded-0 rounded-start" type="search" placeholder="Ex: 12345-678" v-maska="'#####-###'" required autofocus autocomplete="off" v-model="CEP" />
+          <button class="btn btn-success rounded-0 rounded-end border-0" type="button" v-on:click="consult">
+            Consultar
+          </button>
+        </div>
+        <img src="@/assets/detective-img.png" alt="funny icon" width="300" height="200">
       </nav>
 
       <section>
         <div>
           Logradouro
-          <input
-            type="text"
-            class="form-control"
-            placeholder="-----"
-            id="Logradouro"
-            v-model="apiData.logradouro"
-            readonly
-          />
+          <input type="text" class="form-control" placeholder="-----" id="Logradouro" v-model="apiData.logradouro" readonly />
         </div>
 
         <div>
           Complemento
-          <input
-            type="text"
-            class="form-control"
-            placeholder="-----"
-            id="Complemento"
-            v-model="apiData.complemento"
-            readonly
-          />
+          <input type="text" class="form-control" placeholder="-----" id="Complemento" v-model="apiData.complemento" readonly />
         </div>
 
         <div>
           Bairro
-          <input
-            type="text"
-            class="form-control"
-            placeholder="-----"
-            id="Bairro"
-            v-model="apiData.bairro"
-            readonly
-          />
+          <input type="text" class="form-control" placeholder="-----" id="Bairro" v-model="apiData.bairro" readonly />
         </div>
 
         <div>
           Localidade
-          <input
-            type="text"
-            class="form-control"
-            placeholder="-----"
-            id="Localidade"
-            v-model="apiData.localidade"
-            readonly
-          />
+          <input type="text" class="form-control" placeholder="-----" id="Localidade" v-model="apiData.localidade" readonly />
         </div>
 
         <div>
           UF
-          <input
-            type="text"
-            class="form-control"
-            placeholder="-----"
-            id="UF"
-            v-model="apiData.uf"
-            readonly
-          />
+          <input type="text" class="form-control" placeholder="-----" id="UF" v-model="apiData.uf" readonly />
         </div>
 
         <div>
           IBGE
-          <input
-            type="text"
-            class="form-control"
-            placeholder="-----"
-            id="IBGE"
-            v-model="apiData.ibge"
-            readonly
-          />
+          <input type="text" class="form-control" placeholder="-----" id="IBGE" v-model="apiData.ibge" readonly />
         </div>
 
         <div>
           GIA
-          <input
-            type="text"
-            class="form-control"
-            placeholder="-----"
-            id="GIA"
-            v-model="apiData.gia"
-            readonly
-          />
+          <input type="text" class="form-control" placeholder="-----" id="GIA" v-model="apiData.gia" readonly />
         </div>
 
         <div>
           DDD
-          <input
-            type="text"
-            class="form-control"
-            placeholder="-----"
-            id="DDD"
-            v-model="apiData.ddd"
-            readonly
-          />
+          <input type="text" class="form-control" placeholder="-----" id="DDD" v-model="apiData.ddd" readonly />
         </div>
 
         <div>
           SIAFI
-          <input
-            type="text"
-            class="form-control"
-            placeholder="-----"
-            id="SIAFI"
-            v-model="apiData.siafi"
-            readonly
-          />
+          <input type="text" class="form-control" placeholder="-----" id="SIAFI" v-model="apiData.siafi" readonly />
         </div>
       </section>
+
     </div>
     <footer class="d-flex flex-column align-items-center">
       <span>
         Developer by
-        <a href="https://github.com/YoungC0DE" class="text-decoration-none"
-          >Rafael Anjos</a
-        >
+        <a href="https://github.com/YoungC0DE" class="text-decoration-none">Rafael Anjos</a>
       </span>
       <span>YoungC0DE</span>
     </footer>
@@ -157,7 +81,6 @@ export default {
     return {
       apiData: {},
       CEP: null,
-      screenUser: window.innerWidth,
     };
   },
   directives: { maska },
